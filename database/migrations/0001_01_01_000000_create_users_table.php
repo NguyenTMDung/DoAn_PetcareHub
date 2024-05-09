9e662be95d5da6af26e894a871dcc6fc0f84eeef
NguyenTMDung <nguyenthimydung1972004@gmail.com>
2024-05-09 16:45:11 +0700

    add tbl users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('avatar');
             $table->date('date_of_birth');
             $table->string('email')->unique();
             $table->string('phone');

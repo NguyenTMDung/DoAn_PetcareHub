a565dbefd019e93123e582be8982a7fb6015eee7
NguyenTMDung <nguyenthimydung1972004@gmail.com>
2024-05-11 23:26:02 +0700

    add link js

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -15,6 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="{{asset('public/frontend/css/styleHomeadmin.css')}}">
     <link rel="stylesheet" href="{{asset('public/frontend/css/quanlynhanvien.css')}}">
     <style>

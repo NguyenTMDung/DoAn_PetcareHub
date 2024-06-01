aff4e5db30716e64b54451feb549fe2c2368674c
NguyenTMDung <nguyenthimydung1972004@gmail.com>
2024-06-02 00:54:49 +0700

    edit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -24,47 +24,49 @@ class AdminController extends Controller
         if(!$this->checkadmin()) {
             return redirect('/admin-login');
         }
-        $totalRevenue = $this->getTotalRevenue();
-        $totalBills = $this->countTotalBills();
-        $newCustomersCount = $this->countNewCustomersInCurrentMonth();
-        return view('admin.admin_home', [
-            'totalRevenue' => $totalRevenue,
-            'totalBills' => $totalBills,
-            'newCustomersCount' => $newCustomersCount
-        ]);
+        // $totalRevenue = $this->getTotalRevenue();
+        // $totalBills = $this->countTotalBills();
+        // $newCustomersCount = $this->countNewCustomersInCurrentMonth(); 
+        return view('admin.admin_home'
+        // , [
+        //     'totalRevenue' => $totalRevenue,
+        //     'totalBills' => $totalBills,
+        //     'newCustomersCount' => $newCustomersCount
+        // ]
+        );
     }
 
-    <!-- private function getTotalRevenue()
-    {
-        // Sử dụng query builder để gọi function
-        $result = DB::select("SELECT GetTotalRevenue() AS totalRevenue ");
+    //  private function getTotalRevenue()
+    // {
+    //     // Sử dụng query builder để gọi function
+    //     $result = DB::select("SELECT GetTotalRevenue() AS totalRevenue ");
 
-        // Trích xuất kết quả từ kết quả truy vấn
-        $totalRevenue = $result[0]->totalRevenue;
+    //     // Trích xuất kết quả từ kết quả truy vấn
+    //     $totalRevenue = $result[0]->totalRevenue;
 
-        return $totalRevenue;
-    }
+    //     return $totalRevenue;
+    // }
 
-    private function countTotalBills()
-    {
-        // Sử dụng query builder để gọi function
-        $result = DB::select("SELECT CountTotalBills() AS totalBills");
+    // private function countTotalBills()
+    // {
+    //     // Sử dụng query builder để gọi function
+    //     $result = DB::select("SELECT CountTotalBills() AS totalBills");
 
-        // Trích xuất kết quả từ kết quả truy vấn
-        $totalBills = $result[0]->totalBills;
+    //     // Trích xuất kết quả từ kết quả truy vấn
+    //     $totalBills = $result[0]->totalBills;
 
-        return $totalBills;
-    }
-    private function countNewCustomersInCurrentMonth()
-    {
-        // Sử dụng query builder để gọi function
-        $result = DB::select("SELECT CountNewCustomersInCurrentMonth() AS newCustomersCount");
+    //     return $totalBills;
+    // }
+    // private function countNewCustomersInCurrentMonth()
+    // {
+    //     // Sử dụng query builder để gọi function
+    //     $result = DB::select("SELECT CountNewCustomersInCurrentMonth() AS newCustomersCount");
 
-        // Trích xuất kết quả từ kết quả truy vấn
-        $newCustomersCount = $result[0]->newCustomersCount;
+    //     // Trích xuất kết quả từ kết quả truy vấn
+    //     $newCustomersCount = $result[0]->newCustomersCount;
 
-        return $newCustomersCount;
-    } -->
+    //     return $newCustomersCount;
+    // } 
 
     public function Login(){
         return view('pages.login');
--- database/migrations/2024_06_01_175145_create_galleries_table.php
+++ database/migrations/2024_06_01_175145_create_galleries_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('galery', function (Blueprint $table) {
+        Schema::create('galleries', function (Blueprint $table) {
             $table->bigIncrements('id')->primary();
             $table->integer('product_id')->references('id')->on('product')->onDelete('cascade');
             $table->string('file_image');
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('galery');
+        Schema::dropIfExists('galleries');
     }
 };
